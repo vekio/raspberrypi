11d9d4a28dea4bf539a9c6dcd4c2d2efff262590
Alberto Castañeiras <albertocasta@protonmail.com>
2019-11-30 05:19:03 +0100

    Fix git.sh #3
    Fix path problems. Update to v1.1

--- software/git.sh
+++ software/git.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 #
 ##################################################################################################################
-# Version 	: 	1.0
+# Version 	: 	1.1
 # Author 	: 	vekio
 # Purpose   :   install git and set a global config
 ##################################################################################################################
 
 # Use a .env file to set, eg EMAIL=email@example.com
-set -a; [ -f ../.env ] && . ../.env; set +a
+ENVPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. >/dev/null 2>&1 && pwd )"
+set -a; [ -f $ENVPATH/.env ] && . $ENVPATH/.env; set +a
 
 # Install git
 sudo apt-get update && sudo apt-get install -y git
