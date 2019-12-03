02bda30508edca9bf9d55239fb487f4368db1544
vekio <albertocasta@protonmail.com>
2019-12-03 19:22:13 +0100

    Add run_all_containers run docker-compose from anywhere

--- docker/run_all_containers.sh
+++ docker/run_all_containers.sh
@@ -9,7 +9,7 @@
 DOCKERPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Run docker-compose
-docker-compose -f $DOCKERPATH up -d
+docker-compose -f $DOCKERPATH/docker-compose.yml --env-file=$DOCKERPATH/.env up -d
 
 # Run containers script to put files in the volumes
 bash $DOCKERPATH/pihole.sh
