42e8f1dfe033e1af29cdd073e88f5f0f2cc70189
vekio <albertocasta@protonmail.com>
2019-12-03 03:31:19 +0100

    Add fail2ban and openssh #4

--- software/openssh.sh
+++ software/openssh.sh
@@ -9,19 +9,17 @@
 # Install OpenSSH
 sudo apt update && sudo apt install -y openssh-server
 
+# Copy the original sshd_config file
 sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.orig
 
 # PermitRootLogin no
-sed -i '/PermitRootLogin/c\PermitRootLogin no' /etc/ssh/sshd_config
-
+sed -i '/#PermitRootLogin prohibit-password/c\PermitRootLogin no' /etc/ssh/sshd_config
 # PubkeyAuthentication yes
-sed -i '/PubkeyAuthentication/c\PubkeyAuthentication yes' /etc/ssh/sshd_config
-
+sed -i '/#PubkeyAuthentication yes/c\PubkeyAuthentication yes' /etc/ssh/sshd_config
 # PasswordAuthentication no
-sed -i '/PasswordAuthentication/c\PasswordAuthentication no' /etc/ssh/sshd_config
-
+sed -i '/#PasswordAuthentication yes/c\PasswordAuthentication no' /etc/ssh/sshd_config
 # UsePAM no
-sed -i '/UsePAM/c\UsePAM no' /etc/ssh/sshd_config
+sed -i '/UsePAM yes/c\UsePAM no' /etc/ssh/sshd_config
 
 # Restart ssh
 sudo systemctl restart ssh
