c50687097dc99a2f3e709085bb543e0e78247945
vekio <albertocasta@protonmail.com>
2020-02-12 23:25:31 +0100

    Add key script

---
+++ key.sh
@@ -0,0 +1,27 @@
+#/!bin/bash
+
+##################################################################################################################
+# Version	:	1.0
+# Author	:	vekio
+# Purpose	:	this script generate ssh key and it save it in my keybase account, also create a profile in
+#               ssh/config and upload the public key to the server
+# Comments	:	
+##################################################################################################################
+
+# variables
+COMMENT=""          # profile comment
+FNAME=""            # file name
+HNAME=""            # host name
+HIP=""              # host IP or URL
+
+# first run keybase
+run_keybase
+
+# generate ssh key
+ssh-keygen -f /keybase/private/vekio/keys/$FNAME -t rsa -b 4096 -C "" -N ''
+
+# upload the public key to the raspberrypi
+ssh-copy-id -i /keybase/private/vekio/keys/$FNAME.pub pi@$HIP 
+
+# generate .ssh/config profile
+echo -e "# $COMMENT\n  Host $HNAME\n  HostName $HIP\n  User pi\n  IdentifyFile /keybase/private/vekio/keys/$FNAME" | tee -a $HOME/.ssh/config
