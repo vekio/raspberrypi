bb5d189be66eae81a3da4c80ff13c5175411d75b
vekio <albertocasta@protonmail.com>
2020-02-13 19:16:54 +0100

    Comment wiringpi script

--- software/wirinpi.sh
+++ software/wirinpi.sh
@@ -6,6 +6,7 @@
 # Purpose	:	install latest version of wiringpi project
 # Comments	:	http://wiringpi.com/
 #               latest version 2.52: http://wiringpi.com/wiringpi-updated-to-2-52-for-the-raspberry-pi-4b/
+#               check installation with "gpio -v"
 ##################################################################################################################
 
 # install wiringpi
