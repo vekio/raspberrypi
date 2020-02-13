625c77c3391c440356e6fdf3c1fc0e73740131f9
vekio <albertocasta@protonmail.com>
2020-02-13 02:15:05 +0100

    Add fan-controller script

---
+++ software/fan-controller.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+##################################################################################################################
+# Version	:	1.1
+# Author	:	vekio
+# Purpose	:	install a simple fan controller
+# Comments	:	https://howchoo.com/g/ote2mjkzzta/control-raspberry-pi-fan-temperature-python
+##################################################################################################################
+
+# update the package lists
+sudo apt-get update
+
+# install fan controll repository
+cd /tmp
+git clone https://github.com/Howchoo/pi-fan-controller.git
+sudo pip3 install -r pi-fan-controller/requirements.txt
+./pi-fan-controller/script/install
+sudo rm -R pi-fan-controller
