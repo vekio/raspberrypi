6c1c88ea45ad6e914665c6b7bd2b9e2723ab363f
vekio <albertocasta@protonmail.com>
2020-02-13 02:12:46 +0100

    Add docker script

---
+++ software/docker.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+##################################################################################################################
+# Version	:	1.0
+# Author	:	vekio
+# Purpose	:	install latest version of docker-ce and docker-compose 
+# Comments	:	https://docs.docker.com/install/linux/docker-ce/debian/
+#				https://docs.docker.com/compose/install/
+##################################################################################################################
+
+# update the package lists
+sudo apt update
+
+# install docker's package dependencies
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg2 \
+    software-properties-common
+
+# download and add docker's official public pgp key
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+
+# verify the fingerprint
+sudo apt-key fingerprint 0EBFCD88
+
+# add the `stable` channel's Docker upstream repository
+echo "deb [arch=armhf] https://download.docker.com/linux/debian \
+    $(lsb_release -cs) stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list
+
+# update the apt package list (for the new apt repo)
+sudo apt-get update
+
+# install the latest version of docker ce && docker compose
+sudo apt-get install -y --no-install-recommends docker-ce
+
+# allow your user to access the docker cli without needing root access
+sudo usermod -aG docker pi
+
+# install docker-compose using python3 and pip3
+sudo apt-get install -y python3-pip && \
+    sudo pip3 install docker-compose
