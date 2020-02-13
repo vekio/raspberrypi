149830d46d95d7d15bc0d3f7348d020d0fbb5305
vekio <albertocasta@protonmail.com>
2020-02-13 02:12:31 +0100

    Add wiringpi script

---
+++ software/wirinpi.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+##################################################################################################################
+# Version	:	1.0
+# Author	:	vekio
+# Purpose	:	install latest version of wiringpi project
+# Comments	:	http://wiringpi.com/
+#               latest version 2.52: http://wiringpi.com/wiringpi-updated-to-2-52-for-the-raspberry-pi-4b/
+##################################################################################################################
+
+# install wiringpi
+cd /tmp
+wget https://project-downloads.drogon.net/wiringpi-latest.deb
+sudo dpkg -i wiringpi-latest.deb
+rm wiringpi-latest.deb
