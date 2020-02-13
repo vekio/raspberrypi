6b59d03ae24fb5277ba93a74f76f2d557c87b4d2
vekio <albertocasta@protonmail.com>
2020-02-13 02:14:41 +0100

    Add fail2ban script

---
+++ software/fail2ban.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+##################################################################################################################
+# Version	:	1.0
+# Author	:	vekio
+# Purpose	:	install fail2ban
+# Comments	:	
+##################################################################################################################
+
+# update the package lists
+sudo apt-get update
+
+# install fail2ban
+apt-get install -y fail2ban
+
+# copy the original jarl.conf file
+sudo cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
