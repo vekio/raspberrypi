1f8f9db6b6962524513456026613c843e4026c14
vekio <albertocasta@protonmail.com>
2020-02-18 00:49:48 +0100

    Update ssh script

--- config/ssh.sh
+++ config/ssh.sh
@@ -1,31 +1,16 @@
 #!/bin/bash
-
+#
 ##################################################################################################################
-# Version	:	1.0
+# Version	:	1.1
 # Author	:	vekio
 # Purpose	:	changes sshd_config options
-# Comments	:	you can lock the user account but enable PAM
-#               or unlock the user account but disable PAM
 ##################################################################################################################
 
-# variables
-PASSWORD=""       # password, eg: "password123\npassword123"
-
 # bakcup sshd_config file
-sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bk
+sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
 
-sudo sed -i '/#PermitRootLogin prohibit-password/c\PermitRootLogin no' /etc/ssh/sshd_config     # PermitRootLogin no
-sudo sed -i '/#PubkeyAuthentication yes/c\PubkeyAuthentication yes' /etc/ssh/sshd_config        # PubkeyAuthentication yes
-sudo sed -i '/#PasswordAuthentication yes/c\PasswordAuthentication no' /etc/ssh/sshd_config     # PasswordAuthentication no
-sudo sed -i '/#PermitEmptyPasswords no/c\PermitEmptyPasswords no' /etc/ssh/sshd_config          # PermitEmptyPasswords no
-sudo sed -i '/UsePAM yes/c\UsePAM no' /etc/ssh/sshd_config                                      # UsePAM no
+# download sshd_config
+sudo wget -O /etc/ssh/sshd_config https://raw.githubusercontent.com/vekio/raspberrypi/master/config/sshd_config
 
 # restart ssh
 sudo systemctl restart ssh
-sudo service ssh reload
-
-# lock/remove pi's password
-#sudo passwd -l pi
-
-# change password
-echo -e "$PASSWORD" | sudo passwd pi
