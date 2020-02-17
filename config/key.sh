d4f194e3d656995a56181d1efb00ed29f53f0b7f
vekio <albertocasta@protonmail.com>
2020-02-18 00:36:07 +0100

    Update key script

--- config/key.sh
+++ config/key.sh
@@ -1,32 +1,33 @@
-#/!bin/bash
-
+#!/bin/bash
+#
 ##################################################################################################################
-# Version	:	1.0
+# Version	:	1.1
 # Author	:	vekio
-# Purpose	:	this script generate ssh key and it save it in my keybase account, also create a profile in
-#               ssh/config and upload the public key to the server
-# Comments	:	passwd -l, lock the password of the named account, still be able to login with ssh key
-#               ssh profiles: https://www.cyberciti.biz/faq/create-ssh-config-file-on-linux-unix/
+# Purpose	:	this script generate a ssh key and save it in a keybase account,
+#               also create a profile in ssh/config and upload the public key to the server
 ##################################################################################################################
 
 # variables
+USERNAME=""         # user name
 COMMENT=""          # profile comment
-FNAME=""            # file name
-HNAME=""            # host name
-HIP=""              # host IP or URL
+KEYFILE=""          # key file name
+PROFILE=""          # ssh pronfile name
+IP=""               # host IP or URL
+PORT=""             # port to enable ssh
+KEYBASE=""          # keybase path to your keys
 
 # first run keybase
 run_keybase
-sleep 2s
-
-ls /keybase/private/vekio/keys/
+ls $KEYBASE
 
 # generate ssh key
-ssh-keygen -f /keybase/private/vekio/keys/$FNAME -t rsa -b 4096 -C "" -N ''
+ssh-keygen -f $KEYBASE/$KEYFILE -t rsa -b 4096 -C "" -N ''
+
+# remove the know host
+ssh-keygen -f "$HOME/.ssh/known_hosts" -R "$IP"
 
 # upload the public key to the raspberrypi
-ssh-keygen -f "$HOME/.ssh/known_hosts" -R "$HIP"
-ssh-copy-id -i /keybase/private/vekio/keys/$FNAME.pub pi@$HIP 
+ssh-copy-id -i $KEYBASE/$KEYFILE.pub $USERNAME@$IP 
 
 # generate .ssh/config profile
-echo -e "# $COMMENT\nHost $HNAME\n  HostName $HIP\n  User pi\n  IdentityFile /keybase/private/vekio/keys/$FNAME" | tee -a $HOME/.ssh/config
+echo -e "# $COMMENT\nHost $PROFILE\n  HostName $IP\n  Port $PORT\n  User $USERNAME\n  IdentityFile $KEYBASE/$KEYFILE" | tee -a $HOME/.ssh/config
