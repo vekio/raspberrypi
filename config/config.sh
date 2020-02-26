0f4451e4a06d891f4462ebdbfe70ee13a8deb4db
vekio <albertocasta@protonmail.com>
2020-02-26 02:05:07 +0100

    Mount HDD as /home/alberto

--- config/config.sh
+++ config/config.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 #
 ##################################################################################################################
-# Version	:	1.0
-# Author	:	vekio
-# Purpose	:	automate the first steps after install raspbian
-#
-# timezones: https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+# Name			:	config.sh
+# Author		:	vekio
+# Description	:	set up my basic config on raspbian
+# Notes			:	timezones: https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 ##################################################################################################################
+
+
+# config folder path
+CONFIGPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # update && upgrade
 sudo apt-get update && sudo apt-get upgrade -y
@@ -20,21 +23,23 @@ sudo systemctl disable dphys-swapfile
 # timezone
 sudo timedatectl set-timezone Europe/Madrid
 
-# mount hdd
+# mount hdd as /home/alberto
 # check UUID with lsblk -f
 echo "UUID=a8ee6272-205f-4f0f-990a-909844058505 /media/storage ext4 defaults,auto,users,rw,nofail,x-systemd.device-timeout=30 0 0" | sudo tee -a /etc/fstab
+sudo cp -a /home/alberto/. /media/storage/
+sudo rm -r /home/alberto
+sed -i '$ d' /etc/fstab
+echo "UUID=a8ee6272-205f-4f0f-990a-909844058505 /home/alberto ext4 defaults,auto,users,rw,nofail,x-systemd.device-timeout=30 0 0" | sudo tee -a /etc/fstab
 
 # network
 # backup original dhcpcd file
 sudo cp /etc/dhcpcd.conf /etc/dhcpcd.conf.bak
-sudo wget -O /tmp/dhcpcd.conf https://raw.githubusercontent.com/vekio/raspberrypi/master/config/dhcpcd.conf
-sudo cat /tmp/dhcpcd.conf | sudo tee -a /etc/dhcpcd.conf
-sudo rm /tmp/dhcpcd.conf
+sudo cat $CONFIGPATH/dhcpcd.conf | sudo tee -a /etc/dhcpcd.conf
 
 # ssh
 # bakcup sshd_config file
 sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
-sudo wget -O /etc/ssh/sshd_config https://raw.githubusercontent.com/vekio/raspberrypi/master/config/sshd_config
+sudo cat $CONFIGPATH/sshd_config | sudo tee /etc/ssh/sshd_config
 sudo usermod -aG ssh alberto
 sudo systemctl restart ssh
 
